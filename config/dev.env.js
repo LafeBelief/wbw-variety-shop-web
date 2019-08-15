5aaf6c6b7e2b61b86e3386881b3530ac07ae5f72
wangbaowei <wangbaowei@hba.cn>
2019-08-15 15:01:10 +0800

    重新导入 mall ，以此为基础进行扩展

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:9091"'
+  BASE_API: '"http://localhost:8080"'
 })
--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import 'element-ui/lib/index.js'
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import VCharts from 'v-charts'
 
-Vue.use(ElementUI)
-Vue.config.productionTip = false // 阻止你显示显示生产模式的消息
+import '@/styles/index.scss' // global css
 
-/* eslint-disable no-new */
+import App from './App'
+import router from './router'
+import store from './store'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+Vue.use(ElementUI, { locale });
+Vue.use(VCharts)
+
+Vue.config.productionTip = false
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  template: '<App/>',
+  components: { App }
 })
